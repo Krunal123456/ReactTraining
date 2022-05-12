dce1a2cebdc5b8163e46a07ad6a50ad5fb5c456b
krunal.tote <krunal.tote@waycool.in>
2022-05-12 14:47:26 +0530

    day1

--- project/src/App.js
+++ project/src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-
-  const a = 1;
   return (
- <>
-   <button>Click Me</button>
-   <button>Hello</button>
-   <button>{a}</button>
-
- </>
+    <>
+      <section id="hero-section">
+        <div>
+          <h1>
+            Organic plant <br /> and meat products
+          </h1>
+          <button>Discover now</button>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2 id="heading">Food Categories</h2>
+          <p id ="para">
+            The Food and Agriculture Organization of the United Nations
+            (FAO)[Note 1] is a specialized agency of the United Nations that
+            leads international efforts to defeat hunger and improve nutrition
+            and food security. Its Latin motto, fiat panis, translates to "let
+            there be bread". It was founded on 16th October 1945.
+          </p>
+        </div>
+      </section>
+    </>
   );
 }
 
