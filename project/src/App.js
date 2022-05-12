c2fcee40745fa259ec128eebdb29343f15e3d20d
krunal.tote <krunal.tote@waycool.in>
2022-05-12 10:30:08 +0530

    day1

---
+++ project/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const a = 1;
+  return (
+ <>
+   <button>Click Me</button>
+   <button>Hello</button>
+   <button>{a}</button>
+
+ </>
+  );
+}
+
+export default App;
